57bac7a0a64ab0a2b9b59469999f1b8c8fb85b41
Nikita1025 <nekit1025l@gmail.com>
2023-02-04 20:54:01 +0300

    fix Profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 import Mypostcontainer from "./Myposts/Mypostcontainer";
 import {CommonPropsType} from "./ProfileContainer";
-
+import HeaderContainer from "../Header/HeaderContainer";
+import s from './Profile.module.css'
 
 const Profile = (props:CommonPropsType) => {
 
     return (
-        <div>
-            <ProfileInfo  profile={props.profile} status={props.status}
-                          updateStatusThunk={props.updateStatusThunk}/>
-            <Mypostcontainer />
+        <div >
+            <HeaderContainer />
+            <div className={s.container}>
+                <ProfileInfo  profile={props.profile} status={props.status}
+                              updateStatusThunk={props.updateStatusThunk}/>
+                <Mypostcontainer />
+            </div>
+
 
         </div>
 
--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -3,27 +3,21 @@ import Preolader from "../../Comman/Preolader/Preolader";
 import {ProfileInitialStateType} from "../../../Redux/profile-reducer";
 import {ProfileStatusWithHooks} from "../Myposts/ProfileStatusWithHooks";
 import userPhoto from "../../../image/person.png";
+import s from './Profileinfo.module.css'
 
-type ProfileInfoType={
-   profile: ProfileInitialStateType
+type ProfileInfoType = {
+    profile: ProfileInitialStateType
     status: string
-    updateStatusThunk: (status: string)=>void
+    updateStatusThunk: (status: string) => void
 }
 export const ProfileInfo = (props: ProfileInfoType) => {
-    if(!props.profile){
+    if (!props.profile) {
         return <Preolader/>
     }
     return (
-        <div>
-
-            <div>
-                <img
-                    src='https://abrakadabra.fun/uploads/posts/2022-01/1642519550_17-abrakadabra-fun-p-fon-dlya-prezentatsii-sotsseti-23.jpg'
-                    style={{width: "100%"}}
-                />
-            </div>
-            <div>
-                <img src={props.profile.photos.large || userPhoto}/>
+        <div className={s.image}>
+            <div className={s.infoContainer}>
+                <img src={props.profile.photos.large || userPhoto} className={s.avatar}/>
                 <ProfileStatusWithHooks status={props.status} updateStatusThunk={props.updateStatusThunk}/>
             </div>
         </div>
