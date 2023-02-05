526947ea945439c165ba204dda3a67a35db3f666
Nikita1025 <nekit1025l@gmail.com>
2023-02-05 20:54:40 +0300

    fix Nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,28 +1,22 @@
 import React from 'react';
 import s from './Nav.module.css';
-import {NavLink} from "react-router-dom";
+
 const Nav = () =>{
     return(
-        <nav className={s.nav}>
-            <div className={s.item} style={{marginBottom: '15px'}}>
-                <NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>
-            </div>
-            <div className={s.item} style={{marginBottom: '15px'}} >
-                <NavLink to="/dialogs" activeClassName={s.active}>Messages</NavLink>
-            </div>
-            <div className={s.item} style={{marginBottom: '15px'}}>
-                <NavLink to={"/users"} activeClassName={s.active}>Users</NavLink>
-            </div>
-            <div className={s.item} style={{marginBottom: '15px'}}>
-                <a>News</a>
-            </div>
-            <div className={s.item} style={{marginBottom: '15px'}}>
-                <a>Music</a>
-            </div>
-            <div className={s.item} style={{marginBottom: '15px'}}>
-                <a>Settings </a>
-            </div>
-        </nav>
+        <div className={s.container}>
+            <ul className={s.ul}>
+                <li className={s.li} >
+                    <a href="/profile" className={s.a}>Profile</a>
+                </li>
+                <li className={s.li}>
+                    <a href="/dialogs" className={s.a}>Messages</a>
+                </li>
+                <li className={s.li} >
+                    <a href="/users" className={s.a}>Users</a>
+                </li>
+            </ul>
+        </div>
+
     )
 }
 export default Nav;
